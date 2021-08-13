7f1085ba7dbd2489e9af27b9daad2a9b5b9a84b6
Jon Sawyers <tg9569pb@gmail.com>
2021-08-13 15:39:58 -0500

    we have info from the api displaying on the webpage.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios'
+
+
 
 const App = () => {
+  const [characterList, setCharacterList] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacter = () =>{
+      axios.get(`http://swapi.dev/api/people`)
+          .then(res => {
+              console.log(res.data.results)
+              setCharacterList(res.data.results)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+        }
+    fetchCharacter()     
+  }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +32,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterList = {characterList}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+
+const StyledCharacter = styled.div
+`border: 1px solid black;
+background-color:rgba(192,192,192,0.5);
+text-align: center;
+justify-content: center;
+border-radius: 3%;
+width: 400px;
+font-size: 1.6rem;
+
+`
+
+export default function Character(props) {
+    const {characterList} = props
+    
+    const character = characterList.map((char) => {
+        const name = char.name;
+        const gender = char.gender;
+        const height = char.height;
+        const mass = char.mass;
+        const birth = char.birth_year;
+        const eye = char.eye_color;
+        const hair = char.hair_color;
+        const skin = char.skin_color;
+
+        return(
+            <StyledCharacter key={char.id}>
+                <h1>{name}</h1>
+                    <ul>
+                        <p>Gender: {gender}</p>
+                        <p>Height: {height}</p>
+                        <p>Mass: {mass} </p>
+                        <p>BirthYear: {birth}</p>
+                        <p>Eye Color: {eye}</p>
+                        <p>Hair Color: {hair}</p>
+                        <p>Skin Color: {skin}</p>
+                    </ul>
+            </StyledCharacter>
+            )
+
+    });
+    return character
+}
