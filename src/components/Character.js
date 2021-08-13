7894db2c04921ae9420bf8e2c8f091e020d50222
Jon Sawyers <jon-sawyers@lambdastudents.com>
2021-08-13 14:08:31 -0500

    Character app

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const Character = () => {
+
+    const [singleCharacter, setSingleCharacter] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people`)
+            .then(res => {
+                setSingleCharacter(res.data)
+                console.log(res.data)
+            })
+    })
+
+    return (    
+        {singleCharacter}
+    )
+        
+
+};
